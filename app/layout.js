5cf32a9fecc8c195cf6e5d8a1255468f2c987a5f
Web-Online-Concept <web.online.concept@gmail.com>
2025-08-08 12:20:53 +0200

    favicon

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,13 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata = {
   title: 'favoris.pro - Les meilleurs sites pour vos paris sportifs',
   description: 'Sélection des meilleurs sites de paris sportifs',
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon.svg', type: 'image/svg+xml' },
+    ],
+    apple: '/apple-touch-icon.png',
+  },
 };
 
 export default function RootLayout({ children }) {
